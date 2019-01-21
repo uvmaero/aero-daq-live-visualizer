d2260f93485eb73563d487d8bfd700f003305f5c
cullenjemison <cfjemiso@uvm.edu>
2019-01-20 20:53:43 -0500

    [#2 virtual env] Updated run.sh to use virtual environment

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+# if the virtual environment doesn't exist, create it
+if [ ! -e ".venv" ]; then
+    pyvenv .venv
+fi
+
+# activate the virtual environment
+source .venv/bin/activate
+
+# install dependencies
+pip install -r requirements.txt
+
+# run the Twisted app
 twistd -y visualizer.py -n
